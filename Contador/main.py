77c1016c8e2e4b90bde8bee7610bae681861bf9a
mousedesvio <redbrickcar2@gmail.com>
2024-12-13 13:18:52 -0300

    Plot do gráfico e seleção do ID

--- Contador/main.py
+++ Contador/main.py
@@ -1,5 +1,5 @@
-import matplotlib
 import flet as ft
+from flet.matplotlib_chart import MatplotlibChart
 import pandas as pd
 import matplotlib.pyplot as plt
 from matplotlib import style
@@ -7,16 +7,8 @@ import numpy as np
 import os
 
 def main(page: ft.Page):
-    global arquivo
-    arquivo = "Teste"
-
     def on_dialog_result(e: ft.FilePickerResultEvent):
-        print("Selected files:", e.files)
-        print("Selected file or directory:", e.path)
-
-        global arquivo
-        arquivo = e.files[0].name
-        ftArquivo.controls.clear()
+        arquivo.value = e.files[0].path
         page.update()
 
     file_picker = ft.FilePicker(on_result=on_dialog_result)
@@ -25,14 +17,56 @@ def main(page: ft.Page):
 
     btnArquivo = ft.ElevatedButton("Choose file...", on_click=lambda _: file_picker.pick_files())
 
+    arquivo = ft.Text("", size=20)
     ftArquivo = ft.Row([
-                ft.Text(arquivo, size=40),
-                ft.IconButton(icon=ft.icons.SEND, icon_size=20, highlight_color=ft.colors.RED, hover_color=ft.colors.GREY_500, on_click=lambda _: page.go("/results"))
+                arquivo,
+                ft.IconButton(icon=ft.icons.SEND, icon_size=20, highlight_color=ft.colors.RED, hover_color=ft.colors.GREY_500, on_click=lambda e: ViewResults(e))
             ])
     
-    # df = pd.read_csv()
-    # ids = df['Protein ID'].unique()
-    # id = input('Insira o ID da proteína: ')
+    def ViewResults(e):
+        if arquivo.value == "":
+            return
+        
+        page.go("/results")
+
+    def getResults(e, df):
+        '''
+        (int, dataframe) -> None
+        '''
+        id = e.control.value
+        print(id)
+        mask = (df['Protein Accession'].values == id)
+        start = (df['Start'].values)[mask]
+        end = (df['End'].values)[mask]
+
+        contador = np.full(max(end)-min(start)+1, 0)
+
+        for j in range(len(start)):
+            contador[start[j]-1:end[j]] += 1
+
+        linhas = list(range(min(start), max(end)+1))
+        data = {'Line': linhas, 'Count': contador}
+
+        result = pd.DataFrame(data=data)
+
+        figure = plt.figure(figsize=(30,8))
+        print(plt.style.available)
+        #plt.style.use('seaborn-darkgrid')
+
+        plt.bar(linhas, contador, color ='maroon')
+        plt.xlabel('Linha')
+        plt.ylabel('Contagem')
+
+        plt.title(f'Contagem da proteína {id}')
+
+        plt.plot()
+
+        graph.figure = figure
+        page.update()
+
+
+    idField = ft.TextField("", hint_text="Insira o ID da cobertura", on_submit=lambda e: getResults(e, pd.read_csv(f'{arquivo.value}')))
+    graph = MatplotlibChart(plt.figure(figsize=(12,4)), expand=True)
 
     def route_change(route):
         page.views.clear()
@@ -44,6 +78,7 @@ def main(page: ft.Page):
                     btnArquivo,
                     ftArquivo
                 ],
+                bgcolor=ft.Colors.BLUE_200
             )
         )
         if page.route == "/results":
@@ -52,11 +87,13 @@ def main(page: ft.Page):
                     "/store",
                     [
                         ft.AppBar(title=ft.Text("Resultados"), bgcolor=ft.colors.SURFACE_VARIANT),
+                        idField,
                         ft.Row(
                             [
-                                #ft.MatplotlibChart(figure, expand=True)
+                                graph
                             ], width=900)
                     ],
+                    bgcolor=ft.Colors.BLUE_200
                 )
             )
         page.update()
@@ -69,38 +106,5 @@ def main(page: ft.Page):
     page.on_route_change = route_change
     page.on_view_pop = view_pop
     page.go(page.route)
-
-
-def getResults(id, df):
-    '''
-    (int, dataframe) -> figure, dataframe
-    '''
-
-    mask = (df['Protein Accession'].values == id)
-    start = (df['Start'].values)[mask]
-    end = (df['End'].values)[mask]
-
-    contador = np.full(max(end)-min(start)+1, 0)
-
-    for j in range(len(start)):
-        contador[start[j]-1:end[j]] += 1
-
-    linhas = list(range(min(start), max(end)+1))
-    data = {'Line': linhas, 'Count': contador}
-
-    result = pd.DataFrame(data=data)
-
-    figure = plt.figure(figsize=(30,8))
-    plt.style.use('seaborn-darkgrid')
-
-    plt.bar(linhas, contador, color ='maroon')
-    plt.xlabel('Linha')
-    plt.ylabel('Contagem')
-
-    plt.title(f'Contagem da proteína {id}')
-
-    plt.plot()
-
-    return figure, result
 
 ft.app(main)
